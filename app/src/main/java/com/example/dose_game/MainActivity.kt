bb1bb8a68cb2e34afea6230ba1fdc8a67309dbfd
AH.Gandom <ahgm1384@gmail.com>
2025-02-04 21:38:28 +0330

    Designing the home page and setting it to go to the game page

--- app/src/main/java/com/example/dose_game/MainActivity.kt
+++ app/src/main/java/com/example/dose_game/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.dose_game
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dose_game.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView.setOnClickListener(){
+            Toast.makeText(this,"Start Game", Toast.LENGTH_SHORT).show()
+            val intent=Intent(this,PageGame::class.java)
+            startActivity(intent)
+            finish()
+
         }
     }
 }
